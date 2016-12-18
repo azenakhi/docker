df86ec99a171ba65810aa12f47fe8157e890d4b8
Ahmed ZENAKHI <azenakhi@ahmeds-mbp.home>
2016-12-18 15:19:06 +0100

    Added docker run gitlab

---
+++ docker-gitlab.sh
@@ -0,0 +1,9 @@
+docker run --privileged  --detach \
+    --hostname gitlab.example.com \
+    --publish 443:443 --publish 80:80 --publish 2223:22 \
+    --name gitlab \
+    --restart always \
+    --volume /app/list/data/gitlab/config:/etc/gitlab \
+    --volume /app/list/logs/gitlab:/var/log/gitlab \
+    --volume /app/list/data/gitlab:/var/opt/gitlab \
+    gitlab/gitlab-ce:latest
