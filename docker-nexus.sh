62896f83e8eb09abe07f67829c61f51c868c7ef5
azenakhi <ahmed.zenakhi@gmail.com>
2016-12-22 10:44:40 +0100

    Update docker-nexus.sh

--- docker-nexus.sh
+++ docker-nexus.sh
@@ -3,7 +3,7 @@ docker run --detach \
     --publish 18444:18444 \
     --publish 18445:18445 \
     --name nexus \
-    --volume /Users/azenakhi/Downloads/data/nexus:/nexus-data \
+    --volume /app/list/data/nexus:/nexus-data \
     --restart unless-stopped \
     --network intranet \
     sonatype/nexus3
